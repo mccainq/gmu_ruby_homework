ca7bfd61b7251499f093a96117551af3be5bf837
Quentin McCain <qmccain@gmail.com>
2015-04-21 03:53:35 -0400

    Implemented sum_only_numbers and fizzbuzz

--- functions.rb
+++ functions.rb
@@ -1,6 +1,5 @@
 # Return a reversed copy of the array
 def reverse(an_array)
-  # TODO write your code here
   an_array.reverse
 end
 
@@ -8,7 +7,6 @@ end
 # Letters should be lowercase before counting
 # For example, histogram('Hello') returns {'h'=>1,'e'=>1,'l'=>2,'o'=>1}
 def histogram(a_string)
-  # TODO write your code here
   a_string = a_string.downcase
   hist = Hash.new
   a_string.split(//).uniq.each do |str|
@@ -22,7 +20,12 @@ end
 # Be sure to only sum the numbers
 # (Hint: the is_a? method might be useful here)
 def sum_only_numbers(an_array)
-  # TODO write your code here
+  sum = 0
+  an_array.each do |num|
+    next unless num.is_a? Numeric
+    sum = sum + num
+  end
+  sum
 end
 
 # For i from 1 to 100, return an array.
@@ -34,11 +37,24 @@ end
 # For example [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', ..., 14, 'FizzBuzz', ...]
 def fizzbuzz
   # TODO write your code here
+  results = Array.new
+  for count in 1..100
+    if count % 3 == 0 && count % 5 == 0
+      results << 'FizzBuzz'
+    elsif count % 5 == 0
+      results << 'Buzz'
+    elsif count % 3 == 0
+      results << 'Fizz'
+    else
+      results << count
+    end
+  end
+  results
 end
 
 # Uncomment a line to test the function you are developing
 # Type "ruby functions.rb" at command line (Konsole) to run the script
 # puts reverse([3,6,'dog']).inspect
- puts histogram('The Quick brown fox').inspect
+# puts histogram('The Quick brown fox').inspect
 # puts sum_only_numbers [4, 'foo', [ ], 27, :rain, 3.14]
 # puts fizzbuzz.join("\n")
